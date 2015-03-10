bceccaac962f7e9ad19a5520deddd7cd722f9995
Kambiz Behi <kambizbehi@gmail.com>
2015-03-10 17:32:33 -0500

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,42 @@
 import random
 
 def sort(items):
-  # 1. TO DO: Implement a "bubble sort" routine here
-  pass
+    
+    
+    if(len(items) <= 1):
+        return(items)
+
+
+    while(True):
+        
+
+        i = 0
+        number_of_swaps = 0
+
+
+        while(i < len(items)-1):
+
+    
+            if(items[i] > items[i+1]):
+            
+                number_of_swaps += 1
+
+            
+                temp = items[i]
+                items[i] = items[i+1]
+                items[i+1] = temp
+
+            
+            i += 1
+
+    
+        if(number_of_swaps == 0):
+
+            break
+        
+
+    return(items)
+
 
 
 numbers = list(range(10))
@@ -11,6 +45,5 @@ random.shuffle(numbers)
 assert list(range(10)) == sort(numbers)
 print("The list was sorted correctly!")
 
-# 2. Change this print statement to display the complexity category.
-#    Refer to the cheat sheet in week9-class for examples.
-print("This algorithm is classified as: O(....)")
+
+print("This algorithm is classified as: O(n*n)")
